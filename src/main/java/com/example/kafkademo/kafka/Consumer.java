26c6b3f4b35a628c0ea2cd2d713ec060217cb802
huangXin <731191484@qq.com>
2018-08-31 09:14:14 +0800

    Add Producer Consumer SampleMessage

---
+++ src/main/java/com/example/kafkademo/kafka/Consumer.java
@@ -0,0 +1,22 @@
+package com.example.kafkademo.kafka;
+
+/**
+ * @author hx
+ * @Title: Consumer
+ * @ProjectName micro-server-demo
+ * @Description: TODO
+ * @date 2018/8/2220:07
+ */
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @KafkaListener(topics = "testTopic")
+    public void processMessage(SampleMessage message) {
+        System.out.println("Received sample message [" + message + "]");
+    }
+
+}
