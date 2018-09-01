7cf34d3b1af9563cc8b3dae9ad40fc2afa59ce93
huangXin <731191484@qq.com>
2018-09-01 15:59:45 +0800

    Update

---
+++ src/main/java/com/example/kafkademo/kafka/ConsumerB.java
@@ -0,0 +1,22 @@
+package com.example.kafkademo.kafka;
+
+/**
+ * @author hx
+ * @Title: ConsumerB
+ * @ProjectName micro-server-demo
+ * @Description: TODO
+ * @date 2018/8/2220:07
+ */
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component(value = "consumer_topic")
+public class ConsumerB {
+
+    @KafkaListener(topics = "test")
+    public void processMessage(Object message) {
+        System.out.println("Received sample message [" + message + "]");
+    }
+
+}
